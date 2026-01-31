7cf4936409aa68c5c76cae0a9d3445462176bde4
nhatlinhpham45678-dotcom <nhatlinhpham45678@gmail.com>
2026-01-31 09:25:57 +0700

    Add files via upload

---
+++ ECommerce.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ECommerce.Application.DTOs;
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDTO>();
+            CreateMap<CreateProductDTO, Product>();
+            CreateMap<Order, OrderDTO>();
+            CreateMap<OrderItem, OrderItemDTO>();
+            CreateMap<Address, AddressDTO>();
+        }
+    }
+}
