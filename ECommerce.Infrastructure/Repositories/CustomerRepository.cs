55686f6e0534aa7ec650ef4741ba85e176d1d274
nhatlinhpham45678-dotcom <nhatlinhpham45678@gmail.com>
2026-01-31 09:28:05 +0700

    Add files via upload

---
+++ ECommerce.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ECommerce.Domain.Entities;
+using ECommerce.Domain.Repositories;
+using ECommerce.Infrastructure.Persistence;
+
+namespace ECommerce.Infrastructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly ECommerceDbContext _dbContext;
+        public CustomerRepository(ECommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Customer?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Customers
+                .Include(c => c.Address)   // Include owned Address value object
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+        public async Task AddAsync(Customer customer)
+        {
+            await _dbContext.Customers.AddAsync(customer);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
